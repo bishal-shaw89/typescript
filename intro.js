a2259664ed7ec8ae8040dc762ef26f430050de84
bishal-arodek <bishal.shaw@arodek.com>
2023-09-08 19:22:05 +0530

    Intro in typescript

---
+++ intro.js
@@ -0,0 +1,3 @@
+var user = { name: "Bishal", age: 10 };
+console.log("Bishal");
+console.log(user.name);
---
+++ intro.ts
@@ -0,0 +1,6 @@
+let user = { name: "Bishal", age: 10 };
+
+console.log("Bishal");
+console.log(user.name);
+
+export {};
